89c417064c633cda1da6dd77eb8a007e8828c164
hugoiman <hugogiman17@gmail.com>
2020-07-16 12:43:31 +0700

    Tugas 27 Javascript

---
+++ tugas27.js
@@ -0,0 +1,13 @@
+var a = "No";
+var b = " rumah saya ";
+var c = 30;
+var d = "A";
+
+var e = "Jalan pangeran Tirtayarsa No";
+var f = 70;
+
+var hasil = a + b + c + d;
+console.log(hasil);
+
+var hasil2 = e + f;
+console.log(hasil2);
